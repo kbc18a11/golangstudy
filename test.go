653270f59b994cdd5aa311f4f9814e4102d9fe7c
kbc18a11 <wada121400@gmail.com>
2020-10-22 14:08:00 +0900

    何となく配列の使い方が分かったゾ!!

--- test.go
+++ test.go
@@ -4,11 +4,6 @@ import "fmt"
 // fmtモジュールをインポート
 
 func main() { // 最初に実行されるmain()関数を定義
-	const homo = "a"
-
-	age := 26.0
-
-	a := 2
-	age += float64(a)
-	fmt.Println(age)
+	a1 := [...]string{"Red", "Green", "Blue"}
+	fmt.Println(len(a1))
 }
